7f2af2e5a9c646b6b9b5337b7dcecf1543809307
Galih Arghubi_208829 <galih.arghubi@bfi.co.id>
2025-07-03 02:01:27 +0700

    fix: use solid libs on host

--- apps/solid-host/src/app.tsx
+++ apps/solid-host/src/app.tsx
@@ -1,5 +1,6 @@
 import { createResource, lazy, Show, Suspense } from 'solid-js';
 import { ReactWrapper } from './components/ReactWrapper';
+import { SolidLibs } from '@demo/solid-libs';
 
 const SolidRemote = lazy(() => import('solid-remote/SolidRemote'));
 
@@ -7,7 +8,7 @@ export const App = () => {
   const [remote] = createResource(() => import('react-remote/ReactRemote'));
   return (
     <div>
-      <h1>App</h1>
+      <SolidLibs>App</SolidLibs>
       <Suspense>
         <SolidRemote />
       </Suspense>
